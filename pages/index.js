706b42f2ad9ad004aaf3c4629c06e4eaa9aaa344
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-01-08 11:27:40 +0100

    add simple structure for home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
+import React from 'react';
+import fetchHomePageContent from '../fetch/fetchHomePageContent';
+import fetchEvents from '../fetch/fetchEvents';
+import Container from '../components/Container';
 
-const HomePage = () => {
-  return <h1>Vincent Hardaker</h1>
+const HomePage = ({ heading, subheading, background_image }) => {
+  return (
+    <main>
+      <Container bgImage={background_image}>
+        <h1>{heading}</h1>
+        <h2>{subheading}</h2>
+      </Container>
+    </main>
+  );
+};
+
+HomePage.getInitialProps = async () => {
+  const homeRes = await fetchHomePageContent();
+  const calendarRes = await fetchEvents({ pageSize: 3 });
+  console.log(homeRes.data);
+
+  if (homeRes.data) {
+    return { ...homeRes.data };
+  }
+
+  return {};
 };
 
 export default HomePage;
